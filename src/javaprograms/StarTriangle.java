39992defdc38e24223b604856ad45c81a8723662
Yukthashree <yukthashreegr15ganesh@gmail.com>
2026-08-18 12:23:26 +0530

    Updated Java programs

---
+++ src/javaprograms/StarTriangle.java
@@ -0,0 +1,17 @@
+package javaprograms;
+
+
+	public class StarTriangle {
+	    public static void main(String[] args) {
+
+	        for (int i = 1; i <= 5; i++) {
+
+	            for (int j = 1; j <= i; j++) {
+	                System.out.print("* ");
+	            }
+
+	            System.out.println();
+	        }
+	    }
+	}
+
