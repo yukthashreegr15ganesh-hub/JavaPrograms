5a673d454ab04d543b01ead99282931ad4605f12
Yukthashree <yukthashreegr15ganesh@gmail.com>
2026-08-20 13:33:42 +0530

    Updated Java programs

--- src/javaprograms/ArrayLIstDemo1.java
+++ src/javaprograms/ArrayLIstDemo1.java
@@ -18,7 +18,23 @@ public class ArrayLIstDemo1 {
       list.add(null);
       
       System.out.println("Size of ArrayList after adding elements: " + list.size());
-	
+      System.out.println("Arraylist is empty after adding values " + list.isEmpty());
+      System.out.println("Contains of Arryalist after adding values" + list);
+      boolean b1=list.contains("E");
+      System.out.println("Value E is present"+b1);
+      boolean b2=list.contains("R");
+      System.out.println("Value R is present"+b2);
+      list.remove("B");
+      System.out.println("Size of ArrayList after deletion: " + list.size());
+      
+      System.out.println("Contains of Arryalist after deletion" + list);
+      System.out.println("Size of ArrayList after claering: " + list.size());
+      System.out.println("Contains of Arryalist after clearing" + list);
+      
+      
+      
+  	
+  	
 	
 	
 	}
